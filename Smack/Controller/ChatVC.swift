c8b7d1bf4dea676902fa260e75ed40da8a12e7ba
Sebastian C <crossasebastian@gmail.com>
2017-07-29 21:13:06 -0600

    Implemented slide to reveal

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,26 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Sebastian Crossa on 7/29/17.
+//  Copyright © 2017 Sebastian Crossa. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Manually adding an action to the menuButton
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        // Implement gesture recognizers that will help make the reveal smoother
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+}
