1fc92dad45684ea159f7c657319379a051350583
Sebastian C <crossasebastian@gmail.com>
2017-07-31 10:35:16 -0600

    Used SwiftyJSON for JSON parsing

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 // Handle user registration
 class AuthService {
@@ -76,15 +77,22 @@ class AuthService {
         Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
             // JSON parsing
             if response.result.error == nil {
-                if let json = response.result.value as? Dictionary<String, Any> {
-                    if let email = json["user"] as? String {
-                        self.userEmail = email
-                    }
-                    
-                    if let token = json["token"] as? String {
-                        self.authToken = token
-                    }
-                }
+//                if let json = response.result.value as? Dictionary<String, Any> {
+//                    if let email = json["user"] as? String {
+//                        self.userEmail = email
+//                    }
+//
+//                    if let token = json["token"] as? String {
+//                        self.authToken = token
+//                    }
+//                }
+                
+                // Using SwiftyJSON
+                guard let data = response.data else { return } // Making sure data exists
+                let json = JSON(data: data)
+                
+                self.userEmail = json["user"].stringValue // Safely unwraps it as a String for us with .stringValue
+                self.authToken = json["token"].stringValue
                 
                 // Succesfully logged in a user
                 self.isLoggedIn = true
