e8f21455d3446a1e3776894e68a7f52e7138f98a
Sebastian C <crossasebastian@gmail.com>
2017-07-31 21:41:33 -0600

    Added profile images to the collection view

--- Smack/Controller/AvatarPickerVC.swift
+++ Smack/Controller/AvatarPickerVC.swift
@@ -14,6 +14,9 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var segmentControl: UISegmentedControl!
     
+    // Variables
+    var avatarType = AvatarType.dark
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,8 +26,10 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell {
+            cell.configureCell(index: indexPath.item, type: avatarType)
             return cell
         }
+        
         return AvatarCell()
     }
     
@@ -37,11 +42,16 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     @IBAction func segmentControlChanged (_ sender: Any) {
-        
+        if segmentControl.selectedSegmentIndex == 1 {
+            avatarType = AvatarType.light
+            collectionView.reloadData()
+        } else {
+            avatarType = AvatarType.dark
+            collectionView.reloadData()
+        }
     }
     
     @IBAction func backPressed (_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
 }
