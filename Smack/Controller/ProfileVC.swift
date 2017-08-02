4267badf746a1ef844771e6aa46f8752fd7ea52d
Sebastian C <crossasebastian@gmail.com>
2017-08-01 22:19:27 -0600

    Started implementation of profile modal

---
+++ Smack/Controller/ProfileVC.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileVC.swift
+//  Smack
+//
+//  Created by Sebastian Crossa on 8/1/17.
+//  Copyright © 2017 Sebastian Crossa. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func logoutPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        
+    }
+
+}
