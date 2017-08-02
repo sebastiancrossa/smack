35bf3489d5d51e78ea5f8b104ac73ff0040e1abd
Sebastian C <crossasebastian@gmail.com>
2017-08-01 22:44:56 -0600

    Minor changes

--- Smack/Controller/ProfileVC.swift
+++ Smack/Controller/ProfileVC.swift
@@ -41,6 +41,7 @@ class ProfileVC: UIViewController {
         profileImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
         
         let closeTouch = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
     }
     
     @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
